b4b0d2b9c4dddec50e28f36a1bf26c9e9f66d516
ochosteve <ochosteve08@gmail.com>
2024-02-14 12:22:42 +0100

    create cache-aside example folder

--- redis1/app.js
+++ redis1/app.js
@@ -1,45 +1,58 @@
-
-
 const express = require("express");
-const fetch = require("node-fetch");
+const axios = require("axios");
 const redis = require("redis");
+const app = express();
 
-const username = "steve";
+const Username = "steve";
 const port = 4000;
 const redis_port = 6739;
 const client = redis.createClient(redis_port);
 
-function formatOutput(username, numberOfRepos) {}
+function formatOutput(username, numberOfRepos) {
+  return `${username} has ${numberOfRepos} ${
+    numberOfRepos > 1 ? "repos" : "repo"
+  } `;
+}
 
 async function getRepos(req, res) {
-    try{
-      const username = req.params[USERNAME];
-      const response = await fetch(`https://api.github.com/users/${username}`)
-      const {public_repos} = await response.json();
+  try {
+    const username = req.params[Username];
+    const response = await axios.get(
+      `https://api.github.com/users/${username}`
+    );
+    const { public_repos } = await response.json();
 
-      // cache data to redis
-      client.set()
-    }
-    catch(err){
-        console.error(err);
-    }
+    // cache data to redis
+    client.setEx(username, 60, public_repos);
+    res.send(formatOutput(username, public_repos));
+  } catch (err) {
+    console.error(err);
+    res.status(500);
+  }
 }
 
 // cache middleware
 function cache(req, res, next) {
-  const username = req.params[USERNAME];
+  const username = req.params[Username];
+
   client.get(username, (err, data) => {
     if (err) throw err;
+
+    // if data exist in cache
     if (data != null) {
+      console.log("this data is already cached");
       res.send(formatOutput(username, data));
+
+      // else fetch data from database
     } else {
+      console.log("this data is not cached");
       next();
     }
   });
 }
 
-app.get(`/repos/:${username}`, cache, getRepos);
+app.get(`/repos/:${Username}`, cache, getRepos);
 
 app.listen(port, () => {
-  console.log("listening on port ${port}");
+  console.log(`listening on port ${port}`);
 });
